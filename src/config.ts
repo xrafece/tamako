956e3d363c5095aee4e0cb6d5c18d80f7e3de999
xrafece <286402807@qq.com>
2025-05-10 19:21:58 +0800

    [config] change navagater github url

--- src/config.ts
+++ src/config.ts
@@ -45,7 +45,7 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/xrafece", // Internal links should not include the base path, as it is automatically added
+			url: "https://github.com/xrafece/tamako", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
